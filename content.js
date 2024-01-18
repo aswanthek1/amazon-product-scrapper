a63830d6b618e628b0dd2f4b3390749dfc2553aa
Aswanth Raveendran E K <aswanthek1@gmail.com>
2024-01-18 15:29:59 +0530

    Added add to cart functionality and allowed permissions to ebay, etsy, walmart also

--- content.js
+++ content.js
@@ -1,14 +1,62 @@
+function getElementDetails(nameAttr, priceAttr, imgAttr) {
+  const elementArray = []
+  elementArray.push(document.querySelector(nameAttr).textContent.trim());
+  elementArray.push(document.querySelector(priceAttr).textContent.trim());
+  elementArray.push(document.querySelector(imgAttr).src);
+  return elementArray;
+}
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    try {
-        if (request.action === 'scrape') {
-          const name = document.querySelector('#productTitle').textContent.trim();
-          const price = document.querySelector('.a-price-whole').textContent.trim();
-          const image = document.querySelector('#landingImage').src;
-          const url = window.location.href;
-          const data = [{ name, price, image, url }];
-          chrome.runtime.sendMessage({ action: 'displayData', data });
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  try {
+    if (request.action === 'scrape') {
+      console.log(request.url, "action at conetn")
+      // const name = document.querySelector('#productTitle').textContent.trim();
+      // const price = document.querySelector('.a-price-whole').textContent.trim();
+      // const image = document.querySelector('#landingImage').src;
+      let name; let price; let image;
+
+      if (request?.url?.includes('//www.amazon.in')) {
+        // for amazon
+        const elements = getElementDetails(`#productTitle`, `.a-price-whole`, `#landingImage`)
+        name = elements[0]
+        price = elements[1]
+        image = elements[2]
+      }
+      else if (request?.url?.includes('//www.ebay.com/')) {
+        // for ebay
+        const elements = getElementDetails(`.x-item-title__mainTitle span`, `.x-price-primary .ux-textspans`, `.ux-image-carousel-item img`)
+        name = elements[0]
+        price = elements[1]
+        image = elements[2]
+      }
+      else if(request?.url?.includes('//www.etsy.com')) {
+        // for etsy
+        const elements = getElementDetails(`#listing-page-cart .wt-mb-xs-1 h1`, `.wt-text-title-larger`, `.listing-page-image-carousel-component .carousel-image`)
+        name = elements[0]
+        price = elements[1]
+        image = elements[2]
+      }
+      else if(request?.url?.includes('//www.walmart.com/')) {
+        // for walmart
+        const elements = getElementDetails(`#main-title`, `.lh-copy span span`, `div img.noselect`)
+        name = elements[0]
+        price = elements[1]
+        image = elements[2]
+      }
+      const url = window.location.href;
+      const data = [{ name, price, image, url }];
+
+      chrome.storage.local.get("scrappedProducts", (resp) => {
+        if (resp.scrappedProducts && resp.scrappedProducts.length > 0) {
+          const filterdData = resp.scrappedProducts?.filter((item) => (item.name !== data[0].name && item.price !== data[0].price))
+          chrome.storage.local.set({ "scrappedProducts": [...filterdData, data[0]] })
         }
-    } catch (error) {}
-  });
-  
+        else {
+          chrome.storage.local.set({ "scrappedProducts": [data[0]] })
+        }
+      })
+
+      chrome.runtime.sendMessage({ action: 'displayData', data });
+    }
+  } catch (error) { console.log(error, 'at content') }
+});
--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 document.getElementById('scrapeButton').addEventListener('click', function () {
   try {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0]?.id, { action: 'scrape' });
+      chrome.tabs.sendMessage(tabs[0]?.id, { action: 'scrape', url: tabs[0].url });
     });
   } catch (error) { }
 });
@@ -10,7 +10,10 @@ document.getElementById('scrapeButton').addEventListener('click', function () {
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === 'displayData') {
     try {
+      const cartList = document.getElementById('cartList');
+      cartList.style.display = 'none';
       const resultList = document.getElementById('resultList');
+      resultList.style.display = 'block'
       resultList.innerHTML = '';
       request.data.forEach(function (item) {
         const listItem = document.createElement('li');
@@ -21,10 +24,87 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         const link = document.createElement('a');
         link.href = item.url;
         link.setAttribute("target", "_blank")
-        link.textContent = 'View on Amazon';
+        link.textContent = 'View Product';
         listItem.appendChild(link);
         resultList.appendChild(listItem);
       });
     } catch (error) { }
   }
 });
+
+
+document.getElementById('showCartButton').addEventListener('click', function () {
+  try {
+    const resultList = document.getElementById('resultList');
+    resultList.style.display = 'none'
+    const cartList = document.getElementById('cartList');
+    cartList.style.display = 'block';
+    cartList.innerHTML = '';
+    chrome.storage.local.get("scrappedProducts", (resp) => {
+      if(resp.scrappedProducts?.length > 0) {
+        const heading = document.createElement('h3')
+        heading.textContent = 'Cart Items'
+        heading.classList.add('heading_class')
+        cartList.appendChild(heading);
+
+        const clearAllButton = document.createElement('button');
+        clearAllButton.innerText = 'Clear Cart';
+        clearAllButton.style.marginBottom = '20px';
+        cartList.appendChild(clearAllButton);
+        clearAllButton.addEventListener("click", () => {
+          cartList.innerHTML = ''
+          chrome.storage.local.clear()
+          createNoItemTemplate(cartList)
+        })
+
+          resp.scrappedProducts.forEach((item, index) => {
+          const listItem = document.createElement('li');
+          listItem.textContent = `${item.name} - Price: ${item.price}`;
+          const image = document.createElement('img');
+          image.src = item.image;
+          listItem.appendChild(image);
+          const link = document.createElement('a');
+          link.href = item.url;
+          link.setAttribute("target", "_blank")
+          link.textContent = 'View Product';
+          listItem.appendChild(link);
+
+          const clearButton = document.createElement('button');
+          clearButton.id = 'clearButton'
+          clearButton.innerHTML = 'Remove'
+          clearButton.style.float = 'right'
+          listItem.appendChild(clearButton);
+          listItem.id = `${index}`
+          cartList.appendChild(listItem);
+
+          clearButton.addEventListener("click", () => {
+            resp.scrappedProducts.splice(index, 1)
+            const removedProduct = document.getElementById(`${index}`)
+            cartList.removeChild(removedProduct)
+            chrome.storage.local.set({ "scrappedProducts": resp.scrappedProducts})
+            if(resp?.scrappedProducts?.length === 0) {
+              clearAllButton.style.display = 'none';
+              heading.style.display = 'none'
+              createNoItemTemplate(cartList)
+            }
+          })
+      })
+      }
+      else {
+        createNoItemTemplate(cartList)
+      }
+    })
+  } catch (error) { }
+});
+
+
+function createNoItemTemplate(cartList) {
+  const heading = document.createElement('h3')
+  heading.style.textAlign = 'center'
+  heading.style.marginBottom = '20px'
+  heading.textContent = 'Cart Products'
+  const listItem = document.createElement('li');
+  listItem.textContent = `No produts in the cart.`;
+  listItem.prepend(heading)
+  cartList.appendChild(listItem);
+}
